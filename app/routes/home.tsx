d423db66466112c0c2acee885266c9c703f0c8e1
Rishika <sonkerishika05email@gmail>
2025-11-22 15:09:44 +0530

    initial commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Let's Start with Locus" },
+    { name: "description", content: "Welcome to Locus!" },
   ];
 }
 
--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,47 +1,20 @@
-import logoDark from "./logo-dark.svg";
-import logoLight from "./logo-light.svg";
+
 
 export function Welcome() {
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
-      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-        <header className="flex flex-col items-center gap-9">
-          <div className="w-[500px] max-w-[100vw] p-4">
-            <img
-              src={logoLight}
-              alt="React Router"
-              className="block w-full dark:hidden"
-            />
-            <img
-              src={logoDark}
-              alt="React Router"
-              className="hidden w-full dark:block"
-            />
-          </div>
-        </header>
-        <div className="max-w-[300px] w-full space-y-6 px-4">
-          <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
-            <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-              What&apos;s next?
-            </p>
-            <ul>
-              {resources.map(({ href, text, icon }) => (
-                <li key={href}>
-                  <a
-                    className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-                    href={href}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {icon}
-                    {text}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </nav>
+    <main className="flex flex-col items-center justify-center pt-16 pb-4">
+     <div className="max-w-2xl px-4 text-center"></div>
+     <div>
+        <h1 className="mb-6 text-4xl font-bold leading-tight text-gray-900 dark:text-gray-100 sm:text-5xl">
+          Welcome to Locus!
+        </h1>
         </div>
-      </div>
+        <div>
+        <p className="mb-8 text-lg text-gray-700 dark:text-gray-300"> 
+          Locus is your gateway to seamless location tracking and management.
+        </p>
+        </div>
+        
     </main>
   );
 }
@@ -86,4 +59,4 @@ const resources = [
       </svg>
     ),
   },
-];
+]
